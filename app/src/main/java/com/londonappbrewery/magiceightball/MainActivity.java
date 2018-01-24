5e776ee140c585a3c4322d025ce4703347e41ee5
Neo <NEO6666666@GMAIL.COM>
2018-01-25 00:54:55 +0700

    Gradle Plugin & Build Tool Update

--- app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
@@ -1,7 +1,13 @@
 package com.londonappbrewery.magiceightball;
 
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final int[] options = {
+                R.drawable.maybe,
+                R.drawable.no,
+                R.drawable.probably,
+                R.drawable.yes
+        };
+        final ImageView answer = findViewById(R.id.answer);
+
+        Button btnRoll = findViewById(R.id.btnRoll);
+
+        btnRoll.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Random randIntGenerator = new Random();
+                int answrNum = randIntGenerator.nextInt(4);
+                answer.setImageResource(options[answrNum]);
+            }
+        });
     }
 }
